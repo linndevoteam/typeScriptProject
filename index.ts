c8f1c8770c8155fe4137436484aaa53ba400d22f
linnclaesson <linn.denise.claesson@gmail.com>
2025-01-29 09:25:21 +0100

    Restructured to store the response outside the post function

--- index.ts
+++ index.ts
@@ -25,20 +25,47 @@ type PostRequest = {
 }
 
 type MessageData = {
-    role: string,
+    role: string, // "system" | ""
     content: string
+}
+
+type ResponseObject = {
+    id: string,
+    object: string,
+    created: number,
+    model: string,
+    choices: ChoicesData[],
+    usage: object,
+    system_fingerprint: string
+}
+
+type ChoicesData = {
+    index: number,
+    logprobs: null,
+    finish_reason: string,
+    message: MessageData;
+}
+
+let conversationContext: Array<MessageData> = [];
+
+function initContext() {
+    conversationContext.push(createMessageData("system", "You are very helpful assistant"))
+    conversationContext.push(createMessageData("user", "What's the status of the report that I asked for!"))
+}
+
+
+function getAIanswer(aiObject: ResponseObject) : MessageData {
+        console.log(aiObject);
+    let answer : MessageData = aiObject.choices[0].message;
+    return answer;
 }
 
 // funktion för att sätta data som ska postas
 function createPostData() : PostRequest {
-    let postData: PostRequest = {
-        model: "",
-        messages: []
+    const postData: PostRequest = {
+        model: "llama-3.2-3b-instruct",
+        messages: conversationContext 
     };
-    postData.model = "llama-3.2-3b-instruct";
-    postData.messages.push(createMessageData("system", "You are drunk but you are trying to hide it, I'm your boss. Rhyme"))
-    postData.messages.push(createMessageData("user", "What's the status of the report that I asked for!"))
-
     return postData;
 }
 
@@ -46,13 +73,14 @@ function createMessageData(role: string, content: string) : MessageData {
     return {role, content}
 }
 
+initContext();
 // Post request: skicka data till ai och ta ut response
 async function postData(data: PostRequest) {
     //console.log(JSON.stringify(data));
     console.log("---------")
-    console.log(data.messages[0].role + ": " + data.messages[0].content)
+    //console.log(data.messages[0].role + ": " + data.messages[0].content)
 
-    const response = await fetch("https://curly-sloths-raise.loca.lt/v1/chat/completions", {
+    const response = await fetch("https://violet-carrots-stare.loca.lt/v1/chat/completions", {
         method: "POST",
         headers: {
             "Content-Type": "application/json", 
@@ -60,14 +88,29 @@ async function postData(data: PostRequest) {
         body: JSON.stringify(data)
     })
     const responseData = await response.json();
-    const responseArray = responseData.choices;
+    //const responseArray = responseData.choices;
 
     console.log("---------")
 
+    let answer : MessageData = getAIanswer(responseData);
+    console.log(answer);
+    
+
+    /*
     responseArray.forEach((element : any) => {
-        console.log(element.message.role + ": " + element.message.content);
+        const responseRole = element.message.role;
+        const responseContent = element.message.content;
+        const responseAnswer = responseRole + ": " + responseContent;
+        console.log(responseAnswer);
     });
+    */
 
 }
 
 postData(createPostData());
+
+function keepConversation(){
+    let currentConversation = [];
+}
+
+// lagra response --> push in i konversation
